44dc7b2c4d12ff6c61ab5c9a51e72d5a245dc524
chinkowinkoGH <69372404+chinkowinkoGH@users.noreply.github.com>
2021-10-07 23:09:07 +0100

    Add files via upload

--- Login system/Login_system1_4.py
+++ Login system/Login_system1_4.py
@@ -1,36 +1,69 @@
 #imports
-from tkinter import *
+import tkinter as tk
+#from login_system_settings import Custom
 from tkinter import ttk
-
 import sqlite3
 import os
 
-root = Tk()
+screen_size = ("500x500")
 
-class logreg_screen:
-        #First screen 
-        def __init__(self, root ):
-                # Login and register screen
-                self.root = root
-                self.titlesize =("1000x600")
-               
+class LogReg_screen:
 
-        def logregscreen():
-                logreg_root.geometry(self.titlesize)
-                logreg_root.title("Vegetation simulation")
-                Label(text = "Vegetation simulation\nuser portal", background ="grey", foreground ="white" ,height = "2", width = "35").pack()
-                #buttons
-                Label(text="").pack()
-                Button(text = "Login", command = login_screen, height = "1", width = "15",background ="grey", foreground ="white").pack()
+    # Base window or toproot
+    def __init__(self, master):
+        self.master = master
+        self.master.geometry(screen_size)
+        self.frame = tk.Frame(master)
+        # title
+        self.master.title("Vegetation simulation")
+        self.title = tk.Label(text = "Vegetation simulation\nuser portal", background="grey", foreground="white",
+                              height="2", width="35").pack()
 
-                Label(text= "").pack()
-                Button(text = "Register", command = register_screen, height = "1", width = "15", background ="grey", foreground ="white").pack()
-        
-                Label(text="").pack()
-                Button(text="Exit program", command= logreg_root.destroy, height="1", width="15", background ="grey", foreground ="white").pack()
+        # buttons
+        self.space = tk.Label(text="").pack()
+        self.button = tk.Button(text="Login", command = self.openlog_screen(), background="grey", foreground="white",
+                                height="1", width="15").pack()
+        self.space = tk.Label(text="").pack()
+        self.button = tk.Button(text="Register", command=self.openreg_screen, background="grey", foreground="white",
+                                height="1", width="15").pack()
+        self.space = tk.Label(text="").pack()
+        self.button = tk.Button(text="Exit program", command = self.close_screen, background="grey", foreground="white",
+                                height="1", width="15").pack()
+        self.frame.pack()
 
-                logreg_root.mainloop()
-                
+    # functions for buttons to open windows
+    def openreg_screen(self):
+        pass
+    def openlog_screen(self):
+        pass
+
+    # Exit button
+    def close_screen(self):
+        self.master.destroy()
+
+class reg_screen:
+    def __init__(self, master):
+        self.master = master
+        self.frame = tk.Frame(self.master)
+        self.quitButton = tk.Button(self.frame, text = 'Quit', width = 25, command = self.close_windows)
+        self.quitButton.pack()
+        self.frame.pack()
+
+    def close_windows(self):
+        self.master.destroy()
+
+# The main loop where classes are looped.
+def main():
+    root = tk.Tk()
+    LogReg_screen(root)
+    root.mainloop()
+
+# Allowing classes to function
+if __name__ == '__main__':
+    main()
+
+
+
 
         
 
