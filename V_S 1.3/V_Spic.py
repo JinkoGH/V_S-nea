e39d782938ad96d6e6324c3d999f04f2ef232993
chinkowinkoGH <69372404+chinkowinkoGH@users.noreply.github.com>
2022-01-12 23:50:43 +0000

    Add files via upload

---
+++ V_S 1.3/V_Spic.py
@@ -0,0 +1,11 @@
+#V_S pic will hold all image and visual related code including nodes, trees, colours etc.
+
+# Theme colours
+# Sky colours
+day = (83, 151, 207)
+night = (33, 48, 61)
+# collection in the form of list
+sky_group = [day, night]
+
+# colorkeys
+black = (0, 0, 0)
