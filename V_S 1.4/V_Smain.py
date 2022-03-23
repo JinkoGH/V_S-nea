9ba1293b09f9a27bd15f4222ff8a54b7a7c99223
chinkowinkoGH <69372404+chinkowinkoGH@users.noreply.github.com>
2022-03-23 22:55:23 +0000

    Add files via upload

--- V_S 1.4/V_Smain.py
+++ V_S 1.4/V_Smain.py
@@ -8,18 +8,23 @@ from V_Ssim import Sim
 from V_Ssetting import screen_size
 
 def Main():
-    # screen
+    # pygame initialisation
     pg.init()
     pg.mixer.init()
+    # screen
     pg.display.set_caption("Vegetation Simulation")
     screen = pg.display.set_mode(screen_size)
     window_icon = pg.image.load('Images/NEA tree icon.png').convert()
     pg.display.set_icon(window_icon)
     clock = pg.time.Clock()
 
+    # Background music
+    bg_music1 = 'bg music 1.wav'
+    pg.mixer.music.load(bg_music1)
+    pg.mixer.music.play(-1)
+
     # Linking Sim class
     sim = Sim(screen, clock)
-
     # Main program loop
     running = True
     while running:
